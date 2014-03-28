b0e5db3808d5b8c403b89b2b098c5b03622e2ff5
kushal8 <kmit.kushal@gmail.com>
2014-03-28 20:45:21 +0530

    trunk-344

--- web/src/main/java/org/openmrs/web/Listener.java
+++ web/src/main/java/org/openmrs/web/Listener.java
@@ -169,6 +169,7 @@ public final class Listener extends ContextLoader implements ServletContextListe
 				servletContext.setAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE, context);
 				try {
 					Context.openSession();
+					Context.authenticate("admin", "Admin123");
 					PersonName.setFormat(Context.getAdministrationService().getGlobalProperty(
 					    OpenmrsConstants.GLOBAL_PROPERTY_LAYOUT_NAME_FORMAT));
 				}
